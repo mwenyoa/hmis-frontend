dad0e4c1fc772a3c63db517c0a7f757be45e9db2
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-24 15:17:27 +0200

    Define route for about page and system animation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { LoginForm } from "./pages/Login";
 import { Register } from "./pages/Signup";
 import { Footer, Header } from "./components/ui";
+import { AboutUs } from "./pages";
+import AOS from 'aos';
 import "./App.css";
 
 type Props = {};
 
+
 const App: React.FC<Props> = () => {
+  
+useEffect(() =>{
+  AOS.init();
+}, []);
+
   return (
         <Router>
           <Header />
@@ -19,6 +27,7 @@ const App: React.FC<Props> = () => {
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/about" element={<AboutUs />} />
           </Routes>
           </main>
           <Footer />
