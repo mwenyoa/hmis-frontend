6bcad7175343d89ce90a63636be7ff0e454e7fef
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-28 15:09:22 +0200

    ios animation to about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -6,13 +6,23 @@ import { LoginForm } from "./pages/Login";
 import { Register } from "./pages/Signup";
 import { Footer, Header } from "./components/ui";
 import { AboutUs, ContactUs } from "./pages";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./App.css";
+
 
 type Props = {};
 
 
 const App: React.FC<Props> = () => {
   
+  useEffect(() => {
+    AOS.init({
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  }, []); 
+
   return (
         <Router>
           <Header />
--- src/pages/static/about.tsx
+++ src/pages/static/about.tsx
@@ -9,10 +9,10 @@ const AboutUs: React.FC<Props> = () => {
       {/* Introduction Section */}
       <div className="about_intro w-[90%] mx-auto py-4 my-8 grid grid-cols-1 lg:grid-cols-3  rounded-md px-4">
         {/* Image Section */}
-        <div className="about_image col-span-1 bg-[url('src/assets/doctor-2.jpg')] bg-cover bg-no-repeat h-32 sm:h-48 md:h-64 lg:h-72 xl:h-96 rounded shadow-lg"></div>
+        <div className="about_image col-span-1 bg-[url('src/assets/doctor-2.jpg')] bg-cover bg-no-repeat h-32 sm:h-48 md:h-64 lg:h-72 xl:h-96 rounded shadow-lg"  data-aos="fade-up"></div>
 
         {/* Details Section */}
-        <div className="about_details col-span-2 flex flex-col justify-center items-center py-4 mx-10 h-auto">
+        <div className="about_details col-span-2 flex flex-col justify-center items-center py-4 mx-10 h-auto"  data-aos="fade-up">
           <div className="w-full h-36  justify-center items-center  p-4">
           <h2 className="w-full font-bold text-center my-2 text-lg sm:text-xl md:text-2xl text-indigo-400">
             Who We Are
@@ -22,9 +22,9 @@ const AboutUs: React.FC<Props> = () => {
             services.
           </p>
           </div>
-           <div className="w-full h-full  grid lg:grid-cols-2 grid md:grid-cols-2 sm:grid-cols-1 gap-8 py-4 mx-4 h-auto">
+           <div className="w-full h-full  grid lg:grid-cols-2 grid md:grid-cols-2 sm:grid-cols-1 gap-8 py-4 mx-4 h-auto"  data-aos="fade-up">
                 {/* Mission Section */}
-          <div className="cols-span-1 mission sm:w-full mt-6 px-4 shadow-lg rounded-md  bg-white py-4">
+          <div className="cols-span-1 mission sm:w-full mt-6 px-4 shadow-lg rounded-md  bg-white py-5">
             <h3 className="font-bold text-center text-lg sm:text-xl md:text-2xl my-2">
               Our Mission
             </h3>
@@ -35,8 +35,8 @@ const AboutUs: React.FC<Props> = () => {
           </div>
 
           {/* Vision Section */}
-          <div className=" cols-span-1 vision sm:w-full mt-6 px-4 shadow-lg rounded-md  bg-white">
-            <h3 className="font-bold text-center text-lg sm:text-xl md:text-2xl my-2">
+          <div className=" cols-span-1 vision sm:w-full mt-6 px-4 shadow-lg rounded-md  bg-white py-5"  data-aos="fade-up">
+            <h3 className="font-bold text-center text-lg sm:text-xl md:text-2xl my-2"  data-aos="fade-up">
               Our Vision
             </h3>
             <p className="text-center text-sm sm:text-base md:text-lg">
@@ -49,8 +49,8 @@ const AboutUs: React.FC<Props> = () => {
       </div>
 
       {/* Services Section */}
-      <div className="about_services w-[95%] mx-auto py-4 my-4">
-        <h2 className="w-full font-bold text-center my-2 text-lg sm:text-xl md:text-2xl text-indigo-400">
+      <div className="about_services w-[95%] mx-auto py-4 my-4"  data-aos="fade-up">
+        <h2 className="w-full font-bold text-center my-2 text-lg sm:text-xl md:text-2xl text-indigo-400"   data-aos="fade-up">
           Our Services
         </h2>
         <p className="text-center text-sm sm:text-base">
@@ -59,8 +59,8 @@ const AboutUs: React.FC<Props> = () => {
       </div>
 
       {/* Team Section */}
-      <div className="about_services w-[95%] mx-auto py-4 my-4">
-        <h2 className="w-full font-bold text-center my-2 text-lg sm:text-xl md:text-2xl text-indigo-400">
+      <div className="about_services w-[95%] mx-auto py-4 my-4"  data-aos="fade-up">
+        <h2 className="w-full font-bold text-center my-2 text-lg sm:text-xl md:text-2xl text-indigo-400"  data-aos="fade-up">
           Our Team
         </h2>
         <p className="text-center text-sm sm:text-base">
