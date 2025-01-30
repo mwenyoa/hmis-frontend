ddb0ff5cab9a947d1025fdd8c3c067a72c4d5817
mwenyoa <mwenyoaanthony@gmail.com>
2025-01-30 16:09:09 +0200

    Change input field for file upload

--- src/pages/Signup/Register.tsx
+++ src/pages/Signup/Register.tsx
@@ -222,8 +222,8 @@ const Register: React.FC<Props> = () => {
               <input
                 type="file"
                 accept="image/*"
-                name="photo_url"
-                id="photo_url"
+                name="profile_picture"
+                id="profile_picture"
                 required
                 className="block w-full rounded-md bg-white px-3 py-2 text-sm text-gray-900 outline outline-1 outline-gray-300 placeholder-gray-400 focus:ring-2 focus:ring-indigo-600 focus:outline-none"
               />
