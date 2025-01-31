0a739ebdc7b16b8355ee84c0d6d423d42f26040c
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-01-31 17:25:44 +0200

    Add authReducer to app store reducer

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import authReducer from "../reducers/authSlice";
 
 const NODE_ENV: string | undefined = import.meta.env.VITE_APP_NODE_ENV;
 
 const store = configureStore({
-  reducer: {},
+  reducer: { auth: authReducer },
   middleware: (getDefaultMiddleware) =>
     NODE_ENV === "development"
       ? getDefaultMiddleware().concat(logger)
