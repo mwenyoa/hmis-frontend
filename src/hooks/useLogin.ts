48cab6a79f3e81733b3d940d827cc51fbe2aa834
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-02-02 13:35:05 +0200

    Create use login hook

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store/store";
+import { logIn } from "../redux/api/authuser";
+
+type AuthCredentials = {
+  email: string;
+  password: string;
+};
+
+const useLogIn = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { isAuthenticated, isLoading, error } = useSelector((state: RootState) => state.auth);
+
+  // Memoized login function
+  const handleLogin = useCallback(
+    async (credentials: AuthCredentials) => {
+      return await dispatch(logIn(credentials));
+    },
+    [dispatch]
+  );
+
+  return { handleLogin, isAuthenticated, isLoading, error };
+};
+
+export default useLogIn;
