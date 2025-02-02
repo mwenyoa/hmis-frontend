434cabb7126d6888a8024fe76db6532122236dda
Anthony Mwenyo <mwenyoanthony@gmail.com>
2025-02-02 13:39:44 +0200

    Attach auth token to api request header using axios intercepter

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,14 +1,27 @@
 import axios from "axios";
+import store from "../redux/store/store";
 
-const url: string | undefined = import.meta.env.VITE_APP_URL;
+const url: string | undefined = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
-  baseURL: `${url}/api/v1/`,
+  baseURL: `${url}/api`,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
-    Authorization: "Bearer " + localStorage.getItem("token"),
   },
 });
+
+// add token to request headers using axios interceptors
+apiClient.interceptors.request.use(
+  (config) => {
+    const state = store.getState();
+    const token = state.auth.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default apiClient;
